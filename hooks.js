b0358f88a0b4e41913dfbdc344deed88106f7fe6
Willin Wang <willin@willin.org>
2018-05-04 15:31:00 +0800

    debounce

--- hooks.js
+++ hooks.js
@@ -174,11 +174,27 @@
     }
   });
 
-  // 侦听键盘操作
-  body.addEventListener('keyup', function (e) {
+  function debounce(func, wait = 200, immediate = true) {
+    var timeout;
+    return function() {
+      var context = this, args = arguments;
+      var later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  };
+
+  function keyup(e) {
     if (e.target.parentNode.nodeName === 'TD') {
       var form = e.target.parentNode.parentNode.parentNode;
       fillIn(getList(form));
     }
-  });
+  }
+  // 侦听键盘操作
+  body.addEventListener('keyup', debounce(keyup));
 })();
